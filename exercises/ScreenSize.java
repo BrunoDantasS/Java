eaf36a1977b4750387f61e87d0730fa880972704
Bruno Dantas <bruno.wesley77@gmail.com>
2021-02-06 18:28:19 -0300

    Create ScreenSize.java

---
+++ exercises/ScreenSize.java
@@ -0,0 +1,11 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class ScreenSize {
+  public static void main(String[] args) {
+
+    Toolkit tk = Toolkit.getDefaultToolkit();
+    Dimension d = tk.getScreenSize();
+    System.out.print("Screen size: " + d.width + " X " + d.height);
+  }
+}
