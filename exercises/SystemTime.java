17df7e179bf515798ace2fe02a4d2d7b1661a53d
Bruno Dantas <bruno.wesley77@gmail.com>
2021-02-06 18:07:25 -0300

    Create SystemTime.java

---
+++ exercises/SystemTime.java
@@ -0,0 +1,11 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class SystemTime {
+  public static void main(String[] args) {
+    LocalDateTime time = LocalDateTime.now();
+    DateTimeFormatter formatterTime = DateTimeFormatter.ofPattern("HH:mm:ss");
+    String timeFormatted = formatterTime.format(time);
+    System.out.println("Time: " + timeFormatted);
+  }
+}
